1db336f15501192d2d8bedfa1b7405df77d9d142
Alice <fenuwu99@gmail.com>
2023-11-09 14:41:06 +0100

    esercizio_3

---
+++ Esercizio_Tre/Esercizio_Tre/Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.WriteLine("Inserisci 2 valori numerici da sommare");
+
+ var x = Convert.ToInt32(Console.ReadLine());
+ var y = Convert.ToInt32(Console.ReadLine());
+
+var somma = x + y;
+
+Console.WriteLine("Il numero sommato è : " + somma);
